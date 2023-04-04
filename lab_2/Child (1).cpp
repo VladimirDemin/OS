a968faba3bd5f87e24c30e08637398c00e20f8a4
VladimirDemin <95424460+VladimirDemin@users.noreply.github.com>
2023-04-04 10:21:09 -0700

    Add files via upload

---
+++ lab_2/Child (1).cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <conio.h>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int size = atof(argv[0]);
+	unsigned int* array = new unsigned int[size];
+	unsigned int array_element;
+	unsigned int A = atof(argv[argc - 2]);
+	unsigned int B = atof(argv[argc - 1]);
+	int j = 0;
+	for (int i = 2; i < argc - 2; i++)
+	{
+		array_element = atof(argv[i]);
+		if (array_element >= A && array_element <= B)
+		{
+			array[j] = atof(argv[i]);
+			j++;
+		}
+	}
+	for (; j < size; j++)
+	{
+		array[j] = 0;
+	}
+	cout << "new Array:";
+	for (int i = 0; i < size; i++)
+	{
+		cout << array[i]<<" ";
+	}
+	_cputs("Press any key.");
+	_getch();
+
+	delete[] array;
+
+	return 0;
+}
---
+++ lab_2/Parent (1).cpp
@@ -0,0 +1,69 @@
+﻿#define _CRT_SECURE_NO_WARNING
+
+#pragma warning(disable:4996)
+
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    char lpApplicationName[] = "C:\\Users\\HP Pavilion\\source\\repos\\test\\Debug\\Child.exe";
+    char* ch = new char[1];
+    char* lpCommandLine = new char[1];
+
+    lpCommandLine = strcpy(lpCommandLine, "");
+    cout << "Enter size of array: " << endl;
+    int size;
+    cin >> size;
+    ch = _itoa(size, ch, 10);
+    lpCommandLine = strcat(lpCommandLine, ch);
+    lpCommandLine = strcat(lpCommandLine, " ");
+    unsigned int array_element;
+    cout << "Enter array elements: " << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> array_element;
+        ch = _itoa(array_element, ch, 10);
+        lpCommandLine = strcat(lpCommandLine, ch);
+        lpCommandLine = strcat(lpCommandLine, " ");
+    }
+    cout << "Enter A:" << endl;
+    unsigned int A;
+    cin >> A;
+    ch = _itoa(A, ch, 10);
+    lpCommandLine = strcat(lpCommandLine, ch);
+    lpCommandLine = strcat(lpCommandLine, " ");
+
+    cout << "Enter B:" << endl;
+    unsigned int B;
+    cin >> B;
+    ch = _itoa(B, ch, 10);
+    lpCommandLine = strcat(lpCommandLine, ch);
+
+    cout << "Enter X:" << endl;
+    int X;
+    cin >> X;
+
+    STARTUPINFO lpStartUpInfo;
+    ZeroMemory(&lpStartUpInfo, sizeof(STARTUPINFO));
+    lpStartUpInfo.cb = sizeof(STARTUPINFO);
+    lpStartUpInfo.dwFlags = STARTF_USEPOSITION;
+    lpStartUpInfo.dwX = X;
+
+    PROCESS_INFORMATION lpProcessInformation;
+    if (!CreateProcess(lpApplicationName, lpCommandLine, NULL, NULL, FALSE,
+        CREATE_NEW_CONSOLE, NULL, NULL, &lpStartUpInfo, &lpProcessInformation))
+    {
+        _cputs("Press any key.");
+        _getch();
+    }
+
+    WaitForSingleObject(lpProcessInformation.hProcess, INFINITE);
+    CloseHandle(lpProcessInformation.hProcess);
+    CloseHandle(lpProcessInformation.hThread);
+
+    return 0;
+}
