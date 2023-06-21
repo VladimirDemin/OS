0c57e347788f9b54633c34dfe9b88bd813a6b06d
VladimirDemin <95424460+VladimirDemin@users.noreply.github.com>
2023-06-21 09:26:18 +0300

    Update Test4.cpp

--- lab_6/tests/Test4.cpp
+++ lab_6/tests/Test4.cpp
@@ -1,27 +1,30 @@
-//Тестирование работы потока Worker :
+//РўРµСЃС‚РёСЂРѕРІР°РЅРёРµ СЂР°Р±РѕС‚С‹ РїРѕС‚РѕРєР° Worker:
 
 TEST(WorkerTest, ChangingArray) {
-    CRITICAL_SECTION cs;
-    InitializeCriticalSection(&cs);
-    HANDLE hEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
-    short array_data[3][5] = { {5, 2, 0}, {2, 4, 2, 1, 2}, {0} };
-    short** array = new short* [3];
-    for (int i = 0; i < 3; i++) {
+    short array_data[5] = { 5, 2, 0, 2, 4 };
+    short* array = new short[5];
+    for (int i = 0; i < 5; i++) {
         array[i] = array_data[i];
     }
-    HANDLE hThread = (HANDLE)_beginthreadex(NULL, 0, threadWorker, array, 0, NULL);
-    // Ожидаем, пока поток Worker завершит работу
-    while (!started) {
-        Sleep(100);
-    }
-    // Проверяем, что массив был изменен правильно
-    EXPECT_EQ(array[1][0], 2);
-    EXPECT_EQ(array[1][1], 4);
-    EXPECT_EQ(array[1][2], 2);
-    EXPECT_EQ(array[1][3], 2);
-    EXPECT_EQ(array[1][4], 1);
-    CloseHandle(hThread);
-    CloseHandle(hEvent);
-    DeleteCriticalSection(&cs);
+    short** temp_array = new short* [2];
+    temp_array[0] = new short[3];
+    temp_array[0][0] = 5;
+    temp_array[0][1] = 2;
+    temp_array[0][2] = 0;
+    temp_array[1] = array;
+
+    thread t(threadWorker, temp_array, 1);
+    t.join();
+
+    // РџСЂРѕРІРµСЂСЏРµРј, С‡С‚Рѕ РјР°СЃСЃРёРІ Р±С‹Р» РёР·РјРµРЅРµРЅ РїСЂР°РІРёР»СЊРЅРѕ
+    EXPECT_EQ(temp_array[1][0], 2);
+    EXPECT_EQ(temp_array[1][1], 4);
+    EXPECT_EQ(temp_array[1][2], 2);
+    EXPECT_EQ(temp_array[1][3], 2);
+    EXPECT_EQ(temp_array[1][4], 0);
+
     delete[] array;
+    delete[] temp_array[0];
+    delete[] temp_array[1];
+    delete[] temp_array;
 }
