245f16b3236b5bd84967d16f0e95761d97bd3c96
VladimirDemin <95424460+VladimirDemin@users.noreply.github.com>
2023-06-21 08:57:10 +0300

    Update Test_1_Lab1.cpp

--- lab_1/tests/Test_1_Lab1.cpp
+++ lab_1/tests/Test_1_Lab1.cpp
@@ -3,22 +3,22 @@ TEST_CLASS(MainTest)
 public:
     TEST_METHOD(ArrayTest)
     {
-        // Перехватываем stdout
+        // РџРµСЂРµС…РІР°С‚С‹РІР°РµРј stdout
         std::stringstream buffer;
         std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
 
-        // Вводим данные в консоль
+        // Р’РІРѕРґРёРј РґР°РЅРЅС‹Рµ РІ РєРѕРЅСЃРѕР»СЊ
         std::istringstream input("5\n1\n2\n3\n4\n5\n5\n");
         std::cin.rdbuf(input.rdbuf());
 
-        // Запускаем функцию
+        // Р—Р°РїСѓСЃРєР°РµРј С„СѓРЅРєС†РёСЋ
         int result = main();
 
-        // Проверяем вывод
+        // РџСЂРѕРІРµСЂСЏРµРј РІС‹РІРѕРґ
         std::string output = buffer.str();
         Assert::AreEqual(output, "Enter the size of array: Enter the element of the array: Enter the element of the array: Enter the element of the array: Enter the element of the array: Enter the element of the array: Array: 1 2 3 4 5 \nMax element in array is 5\nEnter time you want to stop the thread:");
 
-        // Проверяем результат выполнения функции
+        // РџСЂРѕРІРµСЂСЏРµРј СЂРµР·СѓР»СЊС‚Р°С‚ РІС‹РїРѕР»РЅРµРЅРёСЏ С„СѓРЅРєС†РёРё
         Assert::AreEqual(result, 0);
     }
 };
